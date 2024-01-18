d18f2769727728b45751ab40cd983a07d8b8ea40
GuilhermeMonteiro <guilhermevmonteiro2512@gmail.com>
2024-01-18 20:29:26 -0300

    adicionando alguns desafios

---
+++ Longest_Common_Prefix/java/Solution.java
@@ -0,0 +1,15 @@
+package Longest_Common_Prefix.java;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String[] strs = { "flower", "flow", "flight" };
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        return "";
+    }
+}
---
+++ Valid_Parentheses/Java/Solution.java
@@ -0,0 +1,40 @@
+package Valid_Parentheses.Java;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // String input = "()"; // true
+        // String input = "()[]{}"; // true
+        // String input = "(]"; // false
+        String input = "]"; // false
+        // String input = "(])"; // false
+        System.out.println(isValid(input));
+    }
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> p = new Stack<>();
+        String c = "([{";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (c.contains(String.valueOf(s.charAt(i)))) {
+                p.push(s.charAt(i));
+            } else if(!p.isEmpty()){
+                if (s.charAt(i) == ')' && p.peek() == '(') {
+                    p.pop();
+                } else if (s.charAt(i) == ']' && p.peek() == '[') {
+                    p.pop();
+                } else if (s.charAt(i) == '}' && p.peek() == '{') {
+                    p.pop();
+                } else {
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+        return p.isEmpty();
+    }
+}
